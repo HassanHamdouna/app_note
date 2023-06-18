b647f03622e1a579d6ae721f4b2137c9d9505733
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-18 19:11:18 +0300

    add auth firebase

---
+++ lib/firebase/fb_auth_controller.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FbAuthController {
+  /// 1) singInWithEmailAndPassword
+  /// 2) createAccountWithEmailAndPassword
+  /// 3) createAccountWithEmailAndPassword
+  /// 4) forgetPassword
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+}
