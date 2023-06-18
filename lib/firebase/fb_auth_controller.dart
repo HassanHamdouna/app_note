e8c1cb85255873957c6d361fe4725b9def55e4cb
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-18 19:38:43 +0300

    add function singIn firebase

--- lib/firebase/fb_auth_controller.dart
+++ lib/firebase/fb_auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:app_note/models/fb_response.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FbAuthController {
@@ -6,6 +7,20 @@ class FbAuthController {
   /// 3) createAccountWithEmailAndPassword
   /// 4) forgetPassword
 
-  FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Future<FbResponse> singIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      bool verify = userCredential.user!.emailVerified;
+      if (!userCredential.user!.emailVerified) {
+        await userCredential.user!.sendEmailVerification();
+      }
+      return FbResponse(verify ? 'logged in successfully' : 'Verify your email', verify);
+    } on FirebaseAuthException catch (e) {
+      return FbResponse(e.message ?? 'error', false);
+    } catch (e) {
+      return FbResponse('Something went Wrong', false);
+    }
+  }
 }
