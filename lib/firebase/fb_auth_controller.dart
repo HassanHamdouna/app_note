c1bb5c16089ac90637acfd4bb5006d644c3e1639
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-19 16:31:45 +0300

    add function forgerPassword and currenUser

--- lib/firebase/fb_auth_controller.dart
+++ lib/firebase/fb_auth_controller.dart
@@ -11,12 +11,14 @@ class FbAuthController {
 
   Future<FbResponse> singIn(String email, String password) async {
     try {
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
       bool verify = userCredential.user!.emailVerified;
       if (!userCredential.user!.emailVerified) {
         await userCredential.user!.sendEmailVerification();
       }
-      return FbResponse(verify ? 'logged in successfully' : 'Verify your email', verify);
+      return FbResponse(
+          verify ? 'logged in successfully' : 'Verify your email', verify);
     } on FirebaseAuthException catch (e) {
       return FbResponse(e.message ?? 'error', false);
     } catch (e) {
@@ -24,20 +26,33 @@ class FbAuthController {
     }
   }
 
-  Future<FbResponse> createUser(String email,String password) async {
-    try{
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  Future<FbResponse> createUser(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
       await userCredential.user!.sendEmailVerification();
       return FbResponse('Registered successfully , verify email ', true);
-    }on FirebaseAuthException catch(e){
+    } on FirebaseAuthException catch (e) {
       return FbResponse(e.message ?? 'error', false);
-    } catch(e){
+    } catch (e) {
       return FbResponse('Something went Wrong', false);
     }
   }
 
-  Future<void> signOut(){
-    return _auth.signOut();
+  Future<void> signOut() async {
+    return await _auth.signOut();
   }
 
+  User get currentUser => _auth.currentUser!;
+
+  Future<FbResponse> forgetPassword(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      return FbResponse('Rest email send successfully ', true);
+    } on FirebaseAuthException catch (e) {
+      return FbResponse(e.message ?? "error", false);
+    } catch (e) {
+      return FbResponse('Something went Wrong', false);
+    }
+  }
 }
