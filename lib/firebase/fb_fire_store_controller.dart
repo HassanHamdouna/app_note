195752ec8ae1f0dad66b979cc130efedc9dcd1df
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-20 14:16:15 +0300

    add function delete AND update Notes FirebaseFirestor

--- lib/firebase/fb_fire_store_controller.dart
+++ lib/firebase/fb_fire_store_controller.dart
@@ -3,13 +3,31 @@ import 'package:app_note/models/note.dart';
 import 'package:app_note/utils/firebase_helper.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FbFireStoreController with FirebaseHelper{
+class FbFireStoreController with FirebaseHelper {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<FbResponse> create(Note note) async {
     return _firestore
         .collection('Notes')
         .add(note.toMap())
+        .then((value) => successfullyResponse)
+        .catchError((error) => errorResponse);
+  }
+
+  Future<FbResponse> update(Note note) async {
+    return _firestore
+        .collection('Notes')
+        .doc(note.id)
+        .update(note.toMap())
+        .then((value) => successfullyResponse)
+        .catchError((error) => errorResponse);
+  }
+
+  Future<FbResponse> delete(String id) {
+    return _firestore
+        .collection('Notes')
+        .doc(id)
+        .delete()
         .then((value) => successfullyResponse)
         .catchError((error) => errorResponse);
   }
