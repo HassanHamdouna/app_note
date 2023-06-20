c3c76fc3f1238cb588a8857723acd8566ddf7c4c
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-20 13:40:12 +0300

    add function create Notes FirebaseFirestore

--- lib/firebase/fb_fire_store_controller.dart
+++ lib/firebase/fb_fire_store_controller.dart
@@ -1,3 +1,15 @@
-class FbFireStoreController{
+import 'package:app_note/models/fb_response.dart';
+import 'package:app_note/models/note.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class FbFireStoreController {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<FbResponse> create(Note note) async {
+    return _firestore
+        .collection('Notes')
+        .add(note.toMap())
+        .then((value) => FbResponse('Operation completed successfully', true))
+        .catchError((error) => FbResponse('Operation failed', false));
+  }
 }
