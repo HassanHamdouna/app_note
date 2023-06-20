e5c593d17c1c633c85b0ac7a68c4217873a38bd1
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-20 13:27:58 +0300

    add model Note

---
+++ lib/models/note.dart
@@ -0,0 +1,17 @@
+class Note {
+  late String id;
+  late String title;
+  late String info;
+
+  Note.fromMap(Map<String, dynamic> map) {
+    title = map['title'];
+    info = map['info'];
+  }
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = <String, dynamic>{};
+    map['title'] = title;
+    map['info'] = info;
+    return map;
+  }
+}
