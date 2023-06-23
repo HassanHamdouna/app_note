2e7b75d0c364bd28d83fd6347bd1cdd0644cbc4f
HassanHamdouna <hassan.h.developer@gmail.com>
2023-06-23 21:43:24 +0300

    add BottomSheet and open openGallery AND openComer

--- lib/screens/app/images/upload_images_screen.dart
+++ lib/screens/app/images/upload_images_screen.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UploadImagesScren extends StatefulWidget {
@@ -37,9 +38,20 @@ class _UploadImagesScrenState extends State<UploadImagesScren> {
           children: [
             Expanded(
                 child: _choosePickedImage == null
-                ?  IconButton(onPressed: ()=> openComer(), icon: const Icon(Icons.image,size: 20,color: Colors.black,))
-                :Image.file(File(_choosePickedImage!.path) )
-            ),
+                    ? ConstrainedBox(
+                        constraints: const BoxConstraints(
+                            maxWidth: double.infinity,
+                            minWidth: double.infinity),
+                        child: IconButton(
+                            onPressed: () => _showBottomSheet(context),
+                            icon: const Icon(
+                              Icons.image,
+                              size: 100,
+                              color: Colors.black,
+                            )))
+                    : InkWell(
+                        onTap: () => _showBottomSheet(context),
+                        child: Image.file(File(_choosePickedImage!.path)))),
             ElevatedButton.icon(
                 onPressed: () {},
                 icon: const Icon(Icons.cloud_upload_rounded),
@@ -56,7 +68,6 @@ class _UploadImagesScrenState extends State<UploadImagesScren> {
       ),
     );
   }
-
   void openComer() async{
     XFile? fileImage = await _imagePicker.pickImage(source: ImageSource.camera);
     if(fileImage !=null){
@@ -65,4 +76,96 @@ class _UploadImagesScrenState extends State<UploadImagesScren> {
       });
     }
   }
+
+  void openGallery() async{
+    XFile? fileImage = await _imagePicker.pickImage(source: ImageSource.gallery);
+    if(fileImage !=null){
+      setState(() {
+        _choosePickedImage = fileImage;
+      });
+    }
+  }
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      shape: RoundedRectangleBorder(
+       borderRadius: BorderRadius.only(topRight: Radius.circular(20.r),topLeft: Radius.circular(20.r)),
+      ),
+      clipBehavior: Clip.antiAlias,
+      context: context,
+      builder: (BuildContext context) {
+        return BottomSheet(
+          onClosing: () {
+          },
+          builder: (context) {
+            return Container(
+              color: Colors.white,
+              height: 180,
+              child: Padding(
+                padding:  EdgeInsets.symmetric(horizontal: 14.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Please Choose Image',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,color: Colors.blueGrey),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    InkWell(
+                      onTap: (){
+                        openGallery();
+                        Navigator.pop(context);
+                      },                      child: Row(
+                        children: const[
+                          Expanded(
+                              flex: 1,
+                              child: Icon(Icons.image,color: Colors.black45,)
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Text('From Gallery',
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400,color: Colors.blueGrey),),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    InkWell(
+                      onTap: (){
+                        openComer();
+                        Navigator.pop(context);
+                      },
+                      child: Row(
+                        children: const[
+                          Expanded(
+                              flex: 1,
+                              child: Icon(Icons.camera,color: Colors.black45,)
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Text('From Camera',
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400,color: Colors.blueGrey),),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+
+          },
+        );
+      },
+    );
+  }
+
 }
